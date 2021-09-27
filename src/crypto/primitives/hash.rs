748bc27984fd71fe4d4e5c0aead9054a72198f69
Matteo Monti <matteo.monti@epfl.ch>
2021-09-27 13:36:38 +0200

    Implement traits for `Hash`

--- src/crypto/primitives/hash.rs
+++ src/crypto/primitives/hash.rs
@@ -2,11 +2,15 @@ use blake3::{Hash as BlakeHash, Hasher as BlakeHasher};
 
 use crate::crypto::primitives::errors::{HasherError, SerializeError};
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 use snafu::ResultExt;
 
-pub struct Hash(BlakeHash);
+use std::fmt::{Debug, Display, Error as FmtError, Formatter};
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct Hash(#[serde(with = "SerdeBlakeHash")] BlakeHash);
+
 pub struct Hasher(BlakeHasher);
 
 pub const SIZE: usize = blake3::OUT_LEN;
@@ -42,4 +46,38 @@ where
     let mut hasher = Hasher::new();
     hasher.update(message)?;
     Ok(hasher.finalize())
+}
+
+impl From<[u8; SIZE]> for Hash {
+    fn from(hash: [u8; SIZE]) -> Self {
+        Hash(BlakeHash::from(hash))
+    }
+}
+
+impl Into<[u8; SIZE]> for Hash {
+    fn into(self) -> [u8; SIZE] {
+        self.0.into()
+    }
+}
+
+impl Display for Hash {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Debug for Hash {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "{:?}", self.0)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "BlakeHash")]
+struct SerdeBlakeHash(#[serde(getter = "BlakeHash::as_bytes")] [u8; SIZE]);
+
+impl Into<BlakeHash> for SerdeBlakeHash {
+    fn into(self) -> BlakeHash {
+        BlakeHash::from(self.0)
+    }
 }
