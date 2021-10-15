a35bb11771ae75805c73baef4dda209b7d00227f
Matteo Monti <matteo.monti@epfl.ch>
2021-10-15 11:42:49 +0200

    Make `Message` an auto trait

--- src/unicast/message.rs
+++ src/unicast/message.rs
@@ -4,3 +4,8 @@ pub trait Message:
     'static + Send + Serialize + for<'de> Deserialize<'de>
 {
 }
+
+impl<M> Message for M where
+    M: 'static + Send + Serialize + for<'de> Deserialize<'de>
+{
+}
