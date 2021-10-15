53ca767ac3d866da788dd0f9fafb3de1219e727c
Matteo Monti <matteo.monti@epfl.ch>
2021-10-15 16:01:27 +0200

    Make `Message` `Sync`

--- src/unicast/message.rs
+++ src/unicast/message.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 
 pub trait Message:
-    'static + Send + Serialize + for<'de> Deserialize<'de>
+    'static + Send + Sync + Serialize + for<'de> Deserialize<'de>
 {
 }
 
 impl<M> Message for M where
-    M: 'static + Send + Serialize + for<'de> Deserialize<'de>
+    M: 'static + Send + Sync + Serialize + for<'de> Deserialize<'de>
 {
 }
