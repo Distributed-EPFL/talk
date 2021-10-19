80752c724a87b1269b8ee7ca7a57af523b80900e
Manuel <mvidigueira@gmail.com>
2021-10-19 18:33:53 +0200

    Add `Constant` to `time::sleep_schedules`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

---
+++ src/time/sleep_schedules/constant.rs
@@ -0,0 +1,24 @@
+use crate::time::SleepSchedule;
+
+use std::time::Duration;
+
+#[derive(Debug)]
+pub struct Constant {
+    interval: Duration,
+}
+
+impl Constant {
+    pub fn new(interval: Duration) -> Self {
+        Constant { interval }
+    }
+}
+
+impl SleepSchedule for Constant {
+    fn base(&self) -> Duration {
+        self.interval
+    }
+
+    fn next(&self, _current: Duration) -> Duration {
+        self.interval
+    }
+}
--- src/time/sleep_schedules/mod.rs
+++ src/time/sleep_schedules/mod.rs
@@ -1,3 +1,5 @@
 mod capped_exponential;
+mod constant;
 
 pub use capped_exponential::CappedExponential;
+pub use constant::Constant;
