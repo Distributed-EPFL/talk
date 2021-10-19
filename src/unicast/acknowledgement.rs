7b4e3a8411822d89c6cb3e59c773972d3c55c23b
Manuel <mvidigueira@gmail.com>
2021-10-19 18:39:07 +0200

    Implement `PartialOrd` and `Ord` for `Acknowledgement`
    Co-authored-by: Matteo Monti <matteomonti@users.noreply.github.com>

--- src/unicast/acknowledgement.rs
+++ src/unicast/acknowledgement.rs
@@ -1,8 +1,28 @@
 use serde::{Deserialize, Serialize};
 
+use std::cmp::{Ord, Ordering, PartialOrd};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 #[repr(u8)]
 pub enum Acknowledgement {
     Weak,
     Strong,
+}
+
+impl PartialOrd for Acknowledgement {
+    fn partial_cmp(&self, rho: &Self) -> Option<Ordering> {
+        Some(self.cmp(rho))
+    }
+}
+
+impl Ord for Acknowledgement {
+    fn cmp(&self, rho: &Self) -> Ordering {
+        match (self, rho) {
+            (Acknowledgement::Weak, Acknowledgement::Strong) => Ordering::Less,
+            (Acknowledgement::Strong, Acknowledgement::Weak) => {
+                Ordering::Greater
+            }
+            _ => Ordering::Equal,
+        }
+    }
 }
