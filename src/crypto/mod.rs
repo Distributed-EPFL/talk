5270b2868fe387ffbe6a66f277105b89109be78e
Matteo Monti <matteo.monti@epfl.ch>
2021-10-21 09:52:50 +0200

    Implement `SetHash`

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -1,6 +1,7 @@
 mod key_card;
 mod key_chain;
 mod scope;
+mod set_hash;
 mod statement;
 mod talk_header;
 
@@ -12,4 +13,5 @@ pub use key_card::KeyCard;
 pub use key_card::KeyCardError;
 pub use key_chain::KeyChain;
 pub use scope::Scope;
+pub use set_hash::SetHash;
 pub use statement::Statement;
