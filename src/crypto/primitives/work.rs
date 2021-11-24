0e98ff60cd6b4ca70fb98d914c444b058b1171b4
Matteo Monti <matteo.monti@epfl.ch>
2021-11-24 16:11:11 +0100

    Derive `Clone` for `Work`

--- src/crypto/primitives/work.rs
+++ src/crypto/primitives/work.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 
 use std::convert::TryInto;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Work(u64);
 
 #[derive(Doom)]
