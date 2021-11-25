22973f2b1200c012913cfa4a36d2ad4aee03c1d8
Matteo Monti <matteo.monti@epfl.ch>
2021-11-25 19:21:15 +0100

    Scaffold `SessionConnector`

--- src/net/session_connector.rs
+++ src/net/session_connector.rs
@@ -1,1 +1,47 @@
-pub struct SessionConnector;
+use crate::{
+    crypto::Identity,
+    net::{Connector as NetConnector, SecureConnection, Session},
+    sync::fuse::Fuse,
+};
+
+use doomstack::Stack;
+
+use std::{
+    collections::HashSet,
+    sync::{Arc, Mutex},
+};
+
+use tokio::sync::mpsc::{self, Receiver, Sender};
+
+type ReturnInlet = Sender<SecureConnection>;
+type ReturnOutlet = Receiver<SecureConnection>;
+
+pub struct SessionConnector {
+    connector: Arc<dyn NetConnector>,
+    return_inlet: ReturnInlet,
+    _fuse: Fuse,
+}
+
+impl SessionConnector {
+    pub fn new<C>(connector: C) -> Self
+    where
+        C: NetConnector,
+    {
+        let connector = Arc::new(connector);
+
+        let (return_inlet, _return_outlet) = mpsc::channel(32); // TODO: Add settings
+
+        let fuse = Fuse::new();
+
+        SessionConnector {
+            connector,
+            return_inlet,
+            _fuse: fuse,
+        }
+    }
+
+    async fn connect(&self, remote: Identity) -> Result<Session, Stack> {
+        let connection = self.connector.connect(remote).await?;
+        Ok(Session::new(connection, self.return_inlet.clone()))
+    }
+}
