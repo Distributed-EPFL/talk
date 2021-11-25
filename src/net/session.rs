41b93b9ecda706c5fde593edd88441cdff0ec514
Matteo Monti <matteo.monti@epfl.ch>
2021-11-25 19:11:35 +0100

    Implement `Session`

---
+++ src/net/session.rs
@@ -0,0 +1,61 @@
+use crate::net::{ConnectionSettings, SecureConnection, SecureConnectionError};
+
+use doomstack::Top;
+
+use serde::{Deserialize, Serialize};
+
+use tokio::sync::mpsc::Sender;
+
+type ReturnInlet = Sender<SecureConnection>;
+
+pub struct Session {
+    connection: Option<SecureConnection>,
+    return_inlet: ReturnInlet,
+}
+
+impl Session {
+    pub(in crate::net) fn new(connection: SecureConnection, return_inlet: ReturnInlet) -> Self {
+        Session {
+            connection: Some(connection),
+            return_inlet,
+        }
+    }
+
+    pub fn configure(&mut self, settings: ConnectionSettings) {
+        self.connection.as_mut().unwrap().configure(settings)
+    }
+
+    pub async fn send<M>(&mut self, message: &M) -> Result<(), Top<SecureConnectionError>>
+    where
+        M: Serialize,
+    {
+        self.connection.as_mut().unwrap().send(message).await
+    }
+
+    pub async fn send_plain<M>(&mut self, message: &M) -> Result<(), Top<SecureConnectionError>>
+    where
+        M: Serialize,
+    {
+        self.connection.as_mut().unwrap().send_plain(message).await
+    }
+
+    pub async fn receive<M>(&mut self) -> Result<M, Top<SecureConnectionError>>
+    where
+        M: for<'de> Deserialize<'de>,
+    {
+        self.connection.as_mut().unwrap().receive().await
+    }
+
+    pub async fn receive_plain<M>(&mut self) -> Result<M, Top<SecureConnectionError>>
+    where
+        M: for<'de> Deserialize<'de>,
+    {
+        self.connection.as_mut().unwrap().receive_plain().await
+    }
+}
+
+impl Drop for Session {
+    fn drop(&mut self) {
+        let _ = self.return_inlet.try_send(self.connection.take().unwrap());
+    }
+}
