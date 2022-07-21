6b693d64d5d0ad269121c0a2109c5e7c21034dc5
Matteo Monti <matteo.monti@epfl.ch>
2022-07-21 10:30:28 +0000

    Implement `DatagramSender::send_multiple`

--- src/net/datagram_sender.rs
+++ src/net/datagram_sender.rs
@@ -1,10 +1,20 @@
 use crate::net::Message;
 
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
 use socket2::{Domain, Socket, Type};
 
-use std::{marker::PhantomData, net::SocketAddr};
+use std::{
+    cmp,
+    marker::PhantomData,
+    net::SocketAddr,
+    time::{Duration, Instant},
+};
 
-use tokio::net::{self, ToSocketAddrs, UdpSocket};
+use tokio::{
+    net::{self, ToSocketAddrs, UdpSocket},
+    time,
+};
 
 pub struct DatagramSender<M: Message> {
     socket: UdpSocket,
@@ -48,5 +58,37 @@ where
             .send_to(buffer.as_slice(), destination)
             .await
             .unwrap();
+    }
+
+    pub async fn send_multiple<A>(&self, messages: &[(A, M)], duration: Duration)
+    where
+        A: Sync + ToSocketAddrs,
+    {
+        let messages = messages
+            .par_iter()
+            .map(|(destination, message)| (destination, bincode::serialize(&message).unwrap()))
+            .collect::<Vec<_>>();
+
+        let start = Instant::now();
+        let mut sent = 0;
+
+        while sent < messages.len() {
+            let elapsed = start.elapsed().as_secs_f64() / duration.as_secs_f64();
+
+            let target = (elapsed * (messages.len() as f64)) as usize;
+            let target = cmp::min(target, messages.len());
+
+            for (destination, message) in &messages[sent..target] {
+                self.socket
+                    .send_to(message.as_slice(), destination)
+                    .await
+                    .unwrap();
+            }
+
+            sent = target;
+
+            // TODO: Add settings
+            time::sleep(Duration::from_millis(10)).await;
+        }
     }
 }
