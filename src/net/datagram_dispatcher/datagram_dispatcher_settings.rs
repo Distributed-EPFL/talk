b20a75c26fdb4b098fd3815140f45cc2a306b346
Adrien Matissart <a@matissart.net>
2022-07-27 15:48:06 +0200

    datagram_dispatcher: define default nb of workers based of nb of cpus

--- src/net/datagram_dispatcher/datagram_dispatcher_settings.rs
+++ src/net/datagram_dispatcher/datagram_dispatcher_settings.rs
@@ -15,8 +15,9 @@ pub struct DatagramDispatcherSettings {
 
 impl Default for DatagramDispatcherSettings {
     fn default() -> Self {
+        let cpus = num_cpus::get();
         DatagramDispatcherSettings {
-            workers: 32,
+            workers: std::cmp::max(1, cpus / 4),
             retransmission_delay: Duration::from_millis(100),
             retransmission_interval: Duration::from_millis(10),
             retransmission_batch_size: 500,
