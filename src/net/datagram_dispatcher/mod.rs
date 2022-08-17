3113fcf618cb5c4dde2b7ff4e9a44c900a4668e7
Matteo Monti <matteo.monti@epfl.ch>
2022-08-17 16:50:09 +0200

    Scaffold `DatagramDispatcher::route_in`

--- src/net/datagram_dispatcher/mod.rs
+++ src/net/datagram_dispatcher/mod.rs
@@ -2,9 +2,21 @@ use doomstack::{here, Doom, ResultExt, Top};
 
 use std::{
     io,
-    net::{ToSocketAddrs, UdpSocket},
+    net::{SocketAddr, ToSocketAddrs, UdpSocket},
     sync::Arc,
+    time::Duration,
 };
+
+use tokio::sync::mpsc::{self, Receiver as MpscReceiver, Sender as MpscSender};
+
+type DatagramInlet = MpscSender<(SocketAddr, Message)>;
+type DatagramOutlet = MpscReceiver<(SocketAddr, Message)>;
+
+const MAXIMUM_TRANSMISSION_UNIT: usize = 2048;
+
+// TODO: Turn into settings
+const PROCESS_TASKS: usize = 4;
+const PROCESS_CHANNEL_CAPACITY: usize = 1024;
 
 pub struct DatagramDispatcher {}
 
@@ -13,6 +25,11 @@ pub enum DatagramDispatcherError {
     #[doom(description("Failed to bind address: {:?}", source))]
     #[doom(wrap(bind_failed))]
     BindFailed { source: io::Error },
+}
+
+struct Message {
+    buffer: [u8; MAXIMUM_TRANSMISSION_UNIT],
+    size: usize,
 }
 
 impl DatagramDispatcher {
@@ -27,13 +44,54 @@ impl DatagramDispatcher {
 
         let socket = Arc::new(socket);
 
+        let mut process_inlets = Vec::new();
+        let mut process_outlets = Vec::new();
+
+        for _ in 0..PROCESS_TASKS {
+            let (process_inlet, process_outlet) = mpsc::channel(PROCESS_CHANNEL_CAPACITY);
+
+            process_inlets.push(process_inlet);
+            process_outlets.push(process_outlet);
+        }
+
         {
             let socket = socket.clone();
-            tokio::task::spawn_blocking(move || DatagramDispatcher::receive(socket));
+
+            tokio::task::spawn_blocking(move || {
+                DatagramDispatcher::route_in(socket, process_inlets)
+            });
         }
 
         todo!()
     }
 
-    fn receive(socket: Arc<UdpSocket>) {}
+    fn route_in(socket: Arc<UdpSocket>, process_inlets: Vec<DatagramInlet>) {
+        socket
+            .set_read_timeout(Some(Duration::from_secs(1)))
+            .unwrap(); // TODO: Determine if this call can fail
+
+        for robin in 0.. {
+            let mut buffer = [0u8; MAXIMUM_TRANSMISSION_UNIT];
+
+            let (size, source) = match socket.recv_from(&mut buffer) {
+                Ok((size, source)) => (size, source),
+                Err(error) => {
+                    if error.kind() == io::ErrorKind::WouldBlock
+                        || error.kind() == io::ErrorKind::TimedOut
+                    {
+                        continue;
+                    } else {
+                        panic!("unexpected `ErrorKind` when `recv_from`ing");
+                    }
+                }
+            };
+
+            let message = Message { buffer, size };
+
+            let _ = process_inlets
+                .get(robin % PROCESS_TASKS)
+                .unwrap()
+                .try_send((source, message)); // TODO: Log warning in case of `Err`
+        }
+    }
 }
