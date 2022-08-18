eb38099214d3f218a375d1ec0a408fdcd5d2a394
Matteo Monti <matteo.monti@epfl.ch>
2022-08-18 12:07:06 +0200

    Implement `MessageTable`

--- src/net/datagram_dispatcher/message_table.rs
+++ src/net/datagram_dispatcher/message_table.rs
@@ -3,6 +3,49 @@ use crate::net::datagram_dispatcher::Message;
 use std::collections::VecDeque;
 
 pub(in crate::net::datagram_dispatcher) struct MessageTable {
-    messages: VecDeque<Message>,
+    messages: VecDeque<Option<Message>>,
     offset: usize,
+}
+
+impl MessageTable {
+    pub fn new() -> MessageTable {
+        MessageTable {
+            messages: VecDeque::new(),
+            offset: 0,
+        }
+    }
+
+    pub fn push(&mut self, message: Message) {
+        self.messages.push_back(Some(message));
+    }
+
+    pub fn get(&self, index: usize) -> Option<&Message> {
+        if index >= self.offset {
+            self.messages
+                .get(index - self.offset)
+                .map(Option::as_ref)
+                .flatten()
+        } else {
+            None
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) -> Option<Message> {
+        if index >= self.offset {
+            let message = self
+                .messages
+                .get_mut(index - self.offset)
+                .map(Option::take)
+                .flatten();
+
+            while let Some(None) = self.messages.front() {
+                self.messages.pop_front();
+                self.offset += 1;
+            }
+
+            message
+        } else {
+            None
+        }
+    }
 }
