89451c923cb36afca674edaeb260af3f8c4d19aa
Manuel <mvidigueira@gmail.com>
2022-08-24 16:54:28 +0200

    Update `DatagramDispatcherSettings`

--- src/net/datagram_dispatcher/datagram_dispatcher_settings.rs
+++ src/net/datagram_dispatcher/datagram_dispatcher_settings.rs
@@ -33,7 +33,7 @@ impl Default for DatagramDispatcherSettings {
             process_in_channel_capacity: 4096,
             process_out_channel_capacity: 4096,
             pace_out_datagram_channel_capacity: 16384,
-            pace_out_acknowledgement_channel_capacity: 4096,
+            pace_out_acknowledgement_channel_capacity: 16384,
             pace_out_completion_channel_capacity: 16384,
             route_out_channel_capacity: 16384,
         }
