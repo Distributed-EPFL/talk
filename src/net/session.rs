00574e50e2e5266e48eab6390ad5edcc5e8cae19
Matteo Monti <matteo.monti@epfl.ch>
2022-11-15 15:25:46 +0000

    Implement `bytes` methods for `Session`
    Co-authored-by: Manuel Vidigueira <mvidigueira@users.noreply.github.com>

--- src/net/session.rs
+++ src/net/session.rs
@@ -46,11 +46,22 @@ impl Session {
         self.connection.send(message).await
     }
 
+    pub async fn send_bytes(&mut self, message: &[u8]) -> Result<(), Top<SecureConnectionError>> {
+        self.connection.send_bytes(message).await
+    }
+
     pub async fn send_plain<M>(&mut self, message: &M) -> Result<(), Top<SecureConnectionError>>
     where
         M: Serialize,
     {
         self.connection.send_plain(message).await
+    }
+
+    pub async fn send_plain_bytes(
+        &mut self,
+        message: &[u8],
+    ) -> Result<(), Top<SecureConnectionError>> {
+        self.connection.send_plain_bytes(message).await
     }
 
     pub async fn send_raw<M>(&mut self, message: &M) -> Result<(), Top<SecureConnectionError>>
@@ -74,11 +85,19 @@ impl Session {
         self.connection.receive().await
     }
 
+    pub async fn receive_bytes(&mut self) -> Result<Vec<u8>, Top<SecureConnectionError>> {
+        self.connection.receive_bytes().await
+    }
+
     pub async fn receive_plain<M>(&mut self) -> Result<M, Top<SecureConnectionError>>
     where
         M: DeserializeOwned,
     {
         self.connection.receive_plain().await
+    }
+
+    pub async fn receive_plain_bytes(&mut self) -> Result<Vec<u8>, Top<SecureConnectionError>> {
+        self.connection.receive_plain_bytes().await
     }
 
     pub async fn receive_raw<M>(&mut self) -> Result<M, Top<SecureConnectionError>>
